ddb10fffc052355fa0b76a4fdbcd48cda00d6691
Alex Swiontek <alex@swiontek.io>
2018-02-11 21:22:18 -0800

    remove the terrifying positioning yay

--- src/containers/Logo.js
+++ src/containers/Logo.js
@@ -4,11 +4,13 @@ import wars from './wars.svg';
 import './Logo.css';
 
 const Logo = () => (
-  <div className="starwars-demo">
-    <img src={ star } alt="Star" className="star" />
-    <img src={ wars } alt="Wars" className="wars" />
-    <h2 className="byline" id="byline">Universe Encyclopedia</h2>
-  </div>
+  <section className="flex-parent">
+    <div className="starwars-demo">
+      <img src={ star } alt="Star" className="star" />
+      <img src={ wars } alt="Wars" className="wars" />
+      <h2 className="byline" id="byline">Universe Encyclopedia</h2>
+    </div>
+  </section>
 );
 
 export default Logo;
