7d33ac4a1adf4e0f8d5d2d0cf63528c102aa6176
Alex Swiontek <alex@swiontek.io>
2018-02-12 18:44:05 -0800

    add characters and media

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import stock from '../media/stock.png';
 
-const Card = ({ children, name }) => (
+const Card = ({ children, name, img }) => (
   <article className="mw5 center bg-moon-gray br3 pa3 pa4-ns mv3 ba b--black-10">
     <div className="tc">
-      <img src={ stock } className="br-100 h3 w3 dib" alt="Stock" />
+      <img src={ img || stock } className="br-100 h3 w3 dib" alt="Stock" />
       <h1 className="f5 black-80">{ name }</h1>
       <hr className="mw3 bb bw1 b--black-10" />
     </div>
--- src/containers/People.js
+++ src/containers/People.js
@@ -5,10 +5,49 @@ import SearchBox from '../components/SearchBox';
 import Spinner from '../components/Spinner';
 import ErrorMessage from '../components/ErrorMessage';
 
+import luke from '../media/people/luke.png';
+import c3po from '../media/people/c-3po.png';
+import r2d2 from '../media/people/r2d2.png';
+import vader from '../media/people/vader.png';
+import leia from '../media/people/leia.png';
+import obiwan from '../media/people/obi-wan.png';
+import owen from '../media/people/owen.png';
+import beru from '../media/people/beru.png';
+import r5d4 from '../media/people/r5d4.png';
+import biggs from '../media/people/biggs.png';
+import stock from '../media/stock.png';
+
+const characterImg = (name) => {
+  switch(name) {
+    case 'Luke Skywalker':
+      return luke;
+    case 'C-3PO':
+      return c3po;
+    case 'R2-D2':
+      return r2d2;
+    case 'Darth Vader':
+      return vader;
+    case 'Leia Organa':
+      return leia;
+    case 'Obi-Wan Kenobi':
+      return obiwan;
+    case 'Owen Lars':
+      return owen;
+    case 'Beru Whitesun lars':
+      return beru;
+    case 'R5-D4':
+      return r5d4;
+    case 'Biggs Darklighter':
+      return biggs;
+    default:
+      return stock;
+  }
+}
+
 const PeopleList = ({ people }) => {
   const peopleArray = people.map((person, i) => {
     return (
-      <Card key={ i } name={ person.name }>
+      <Card key={ i } name={ person.name } img={ characterImg(person.name) }>
         <Entry keyName="BIRTH YEAR" value={ person.birth_year } />
         <Entry keyName="HEIGHT" value={ (person.height * 0.0328084).toFixed(1) + ' ft' } />
         <Entry keyName="WEIGHT" value={ Math.round(person.mass * 2.20462) + ' lbs' } />
