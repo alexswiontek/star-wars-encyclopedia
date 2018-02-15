5330f37a694841b8a7e4541e24908fc5a4d0dd5b
Alex Swiontek <alex@swiontek.io>
2018-02-14 18:32:29 -0800

    css fixez

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,9 +6,9 @@ const Card = ({ children, name, img }) => (
     <div className="tc">
       <img src={ img || stock } className="br-100 h3 w3 dib" alt="Stock" />
       <h1 className="f5 black-80">{ name }</h1>
-      <hr className="mw3 bb bw1 b--black-10" />
+      <hr style={{ margin: '0 auto' }} className="tc mw3 bb bw1 b--black-10" />
     </div>
-    <div className="lh-copy measure center f6 black-70">
+    <div className="pa2 lh-copy measure center f6 black-70">
       { children }
     </div>
   </article>
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Entry = ({ keyName, value }) => (
-  <p style={{ fontFamily: 'Verdana', fontWeight: 'bold', letterSpacing: '0.06em', textAlign: 'left' }}>
+  <p style={{ fontFamily: 'Verdana', fontWeight: 'bold', letterSpacing: '0.06em', textAlign: 'left', paddingBottom: '5px' }}>
     <span>{ keyName }</span>
     <span>{ `: ` }</span>
     <span style={{ color: '#426be8' }}>{ value }</span>
