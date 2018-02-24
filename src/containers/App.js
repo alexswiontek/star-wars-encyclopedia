3795be4202b364949129a475c02f46755c201a6f
Alex Swiontek <alex@swiontek.io>
2018-02-23 20:55:01 -0800

    add particles.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,59 +8,66 @@ import Starships from './Starships';
 import Vehicles from './Vehicles';
 import Logo from '../components/Logo';
 import Back from '../components/Back';
+import Particles from 'react-particles-js';
+import ParticleOptions from './ParticleOptions';
 import './App.css';
 
 const Home = () => (
-  <div id="home">
-      <Link to="/films">Films</Link>
-      <Link to="/people">People</Link>
-      <Link to="/planets">Planets</Link>
-      <Link to="/species">Species</Link>
-      <Link to="/starships">Starships</Link>
-      <Link to="/vehicles">Vehicles</Link>
-  </div>
+	<div id="home">
+			<Link to="/films">Films</Link>
+			<Link to="/people">People</Link>
+			<Link to="/planets">Planets</Link>
+			<Link to="/species">Species</Link>
+			<Link to="/starships">Starships</Link>
+			<Link to="/vehicles">Vehicles</Link>
+	</div>
 );
 
 const Header = ({ location }) => (
-  <div id="header">
-    { location.pathname !== '/' && <Back /> }
-    <h1 className="title inline-flex tracked-mega">{ location.pathname.replace(/\W/, '').toUpperCase() }</h1>
-    <div className="hidden"><Back /></div>
-  </div>
+	<div id="header">
+		{ location.pathname !== '/' && <Back /> }
+		<h1 className="title inline-flex tracked-mega">{ location.pathname.replace(/\W/, '').toUpperCase() }</h1>
+		<div className="hidden"><Back /></div>
+	</div>
 );
 
 const ErrorPage = () => (
-  <div>
-    <p>Oh snap! Something bad happened. You should go home.</p>
-    <div className="flex-center">
-      <Link to="/" className="obvious">Home</Link>
-    </div>
-  </div>
+	<div>
+		<p>Oh snap! Something bad happened. You should go home.</p>
+		<div className="flex-center">
+			<Link to="/" className="obvious">Home</Link>
+		</div>
+	</div>
 );
 
 const App = () => (
-  <Router basename="star-wars-encyclopedia">
-    <section>
-      <div className="stars"></div>
-      <div className="twinkling"></div>
-      <Logo />
-      <Route path="/:title" component={ Header }/>
+	<Router basename="star-wars-encyclopedia">
+		<section>
+			<Particles
+				params={{
+          particles: ParticleOptions.particles,
+          interactivity: ParticleOptions.interactivity
+        }}
+        className="particles-js"
+			/>
+			<Logo />
+			<Route path="/:title" component={ Header }/>
 
-      <article id="main-content">
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route path="/films" component={ Films } />
-          <Route path="/people" component={ People } />
-          <Route path="/planets" component={ Planets } />
-          <Route path="/species" component={ Species } />
-          <Route path="/starships" component={ Starships } />
-          <Route path="/vehicles" component={ Vehicles } />
-          <Route path="/404" component={ ErrorPage } />
-          <Redirect from="/*" to="/404" />
-        </Switch>
-      </article>
-    </section>
-  </Router>
+			<article id="main-content">
+				<Switch>
+					<Route exact path="/" component={ Home } />
+					<Route path="/films" component={ Films } />
+					<Route path="/people" component={ People } />
+					<Route path="/planets" component={ Planets } />
+					<Route path="/species" component={ Species } />
+					<Route path="/starships" component={ Starships } />
+					<Route path="/vehicles" component={ Vehicles } />
+					<Route path="/404" component={ ErrorPage } />
+					<Redirect from="/*" to="/404" />
+				</Switch>
+			</article>
+		</section>
+	</Router>
 );
 
 export default App;
